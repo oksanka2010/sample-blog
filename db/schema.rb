88ef8ee3b68ab3439c712553515de1d0751e3b8d
Oksana Kamenchuk <nofelet069@gmail.com>
2024-12-22 10:54:55 +0300

    Add one-to-many article-comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_24_061629) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_21_040220) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -24,4 +33,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_24_061629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "articles"
 end
