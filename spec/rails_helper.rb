5bbcbb2b66697bf2509a2c14958213b58b127ffc
Oksana Kamenchuk <nofelet069@gmail.com>
2025-02-28 06:58:23 +0300

    Add factory bot configuration for rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,6 +17,10 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
